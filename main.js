fc641a6fc25efdfb7ca0f570aa52f04b82dbe369
shinmototaihei <arapon4382@gmail.comarapon4382@gmail>
2021-01-20 18:39:53 +0900

    check

---
+++ main.js
@@ -0,0 +1,36 @@
+console.log("Hello world");
+let say = "Hello";
+console.log(say);
+say = "goodbye";
+console.log(say);
+
+const per = 5 % 2;
+console.log(per);
+let inc = 2;
+inc++;
+console.log(inc);
+
+let dec = 2;
+dec--;
+console.log(dec);
+
+const a = 2;
+const b = 2;
+console.log(a == b);
+
+const price = 1800;
+
+if (price < 1000) {
+  console.log("安い");
+} else if (price > 2000) {
+  console.log("高い");
+} else {
+  console.log("丁度良い");
+}
+
+const price = 1000;
+const tax = 0.1;
+console.log(price * tax);
+
+
+
